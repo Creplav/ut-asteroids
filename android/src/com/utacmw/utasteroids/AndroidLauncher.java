731c378d0f1c5364295f254af34a1161b5d25058
Creplav <creplav@gmail.com>
2017-10-18 21:12:04 -0400

    Added base project files

---
+++ android/src/com/utacmw/utasteroids/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.utacmw.utasteroids;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.utacmw.utasteroids.UtAsteroidsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UtAsteroidsGame(), config);
+	}
+}
