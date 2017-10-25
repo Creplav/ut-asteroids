22e5bd8d4772879f4f1031e4e548059556384eef
Yizhen Shi <yizhenshi@Yizhens-MacBook-Air.local>
2017-10-24 23:52:22 -0400

    Added bullet--final version

--- core/src/com/utacmw/utasteroids/GameOverOverlay.java
+++ core/src/com/utacmw/utasteroids/GameOverOverlay.java
@@ -1,13 +1,18 @@
 package com.utacmw.utasteroids;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.*;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+
 /**
- * Created by ben on 10/24/17.
+ * Game over overlay
+ * @author Ben
  */
 
 public class GameOverOverlay {
@@ -16,16 +21,22 @@ public class GameOverOverlay {
 
     public GameOverOverlay() {
         this.viewport = new ExtendViewport(200, 200);
-        font = new BitmapFont();
-        font.setColor(Color.WHITE);
-        font.getData().setScale(1);
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("dangerflight.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 24;
+        parameter.color = Color.WHITE;
+        font = generator.generateFont(parameter);
+        generator.dispose();
     }
 
+    public void dispose() {
+        font.dispose();
+    }
     public void render(SpriteBatch batch){
         viewport.apply(true);
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
-        font.draw(batch, "GAME OVER", viewport.getScreenWidth() / 2, viewport.getScreenHeight() / 2);
+        font.draw(batch, "GAME OVER", viewport.getScreenWidth() / 2, viewport.getScreenHeight() / 2, 0, Align.center, false);
         batch.end();
     }
 }
