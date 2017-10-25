5c550fa6ccfdb953a53a26f9f6a5a784c0dfbeea
Yizhen Shi <yizhenshi@Yizhens-MacBook-Air.local>
2017-10-24 22:40:01 -0400

    Added bullet--better one

--- core/src/com/utacmw/utasteroids/Bullet.java
+++ core/src/com/utacmw/utasteroids/Bullet.java
@@ -1,8 +1,101 @@
 package com.utacmw.utasteroids;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+
+
 /**
  * Created by yizhenshi on 10/24/17.
  */
 
 public class Bullet {
+
+    //variables for the bullet
+    private float lifeTime;
+    private float lifeTimer;
+
+
+
+
+    Viewport viewport;
+
+    Rectangle hitbox;
+    private boolean remove;
+
+    private final float speed = 200;
+
+    private Sprite sprite;
+    private Vector2 position;
+    private Player player;
+    private Texture text;
+
+
+
+    public Bullet(Viewport viewport, Player player){
+        this.player = player;
+        sprite = new Sprite(new Texture("fireball.png"));
+        this.viewport = viewport;
+
+
+
+
+
+        sprite.setSize(10, 10);
+        lifeTime = 1;
+        lifeTimer = 0;
+
+        position = new Vector2();
+        this.position.x = this.sprite.getX();
+        this.position.y = this.sprite.getY();
+
+
+        position.x = this.player.getPosition().x + this.player.getHeight()/2- this.sprite.getWidth()/2;
+        position.y = this.player.getPosition().y + this.player.getWidth()/2 - this.sprite.getWidth()/2;
+
+    }
+
+
+    private void move(float delta){
+
+
+        position.x += -speed * delta * (float)Math.sin(Math.toRadians(player.getRotation()));
+        position.y += speed * delta * (float)Math.cos(Math.toRadians(player.getRotation()));
+        this.sprite.setPosition(this.position.x, this.position.y);
+
+    }
+
+
+    public boolean shouldRemove(){ return remove; }
+
+
+
+    public void update(float delta){
+        move(delta);
+
+        lifeTimer += delta;
+        if (lifeTimer > lifeTime){
+            remove = true;
+        }
+    }
+
+
+
+    public void draw(SpriteBatch batch){
+        sprite.draw(batch);
+    }
+
+
+
+
+
+
+
 }
--- core/src/com/utacmw/utasteroids/Player.java
+++ core/src/com/utacmw/utasteroids/Player.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.utils.DelayedRemovalArray;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import java.util.ArrayList;
+
 /**
  * Class for the player
  * @author Ben
@@ -35,11 +37,15 @@ public class Player {
     private boolean invincible;
     public boolean isDestroyed = false;
 
+
+    private ArrayList<Bullet> bullets;
+
+
     /**
      * Creates a new player
      * @param viewport
      */
-    public Player(Viewport viewport){
+    public Player(Viewport viewport, ArrayList<Bullet> bullets){
         // Set starting lives to 3
         lives = 3;
         // Set initial speed to 0
@@ -66,6 +72,8 @@ public class Player {
                 viewport.getScreenHeight() / 2 - this.sprite.getHeight() / 2);
         //set bound
         //viewport.setScreenBounds(0,0,200,200);
+
+        this.bullets = bullets;
 
         //Sets the player's position to the sprite's x and y value
         this.position.x = sprite.getX();
@@ -122,10 +130,8 @@ public class Player {
             speed = MIN_SPEED;
     }
 
-    private void shoot(){
-        //TODO Set up the shooting
 
-    }
+
 
     /**
      * Moves the player in the direction it is facing
@@ -185,9 +191,8 @@ public class Player {
             move();
         }
         else decelerate();
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
-            shoot();
-        }
+
+
 
         //TODO Add in the hardware values to control movement and shooting
     }
@@ -199,6 +204,7 @@ public class Player {
         // Draw a temp image in the center of the screen
         if(lives > 0)
             sprite.draw(batch);
+
     }
 
     public void wrap(){
@@ -217,4 +223,8 @@ public class Player {
             sprite.setY(200);
         }
     }
+
+    public float getRotation(){return rotation;}
+    public float getHeight(){return this.sprite.getHeight();}
+    public float getWidth(){return this.sprite.getWidth();}
 }
